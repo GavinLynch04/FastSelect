317abcaa38ddfe386943ad320aea8ca8ca6791d8
galynch <gavinlynch04@gmail.com>
2025-07-18 07:08:30 -0700

    getting tests and CI/CD working

--- __init__.py
+++
@@ -1,6 +0,0 @@
-from src.fast_select.ReliefF import ReliefF
-from src.fast_select.SURF import SURF
-from src.fast_select.MultiSURF import MultiSURF
-from src.fast_select.chi2 import chi2
-
-__all__ = ['ReliefF', 'SURF', 'MultiSURF', 'chi2']
---
+++ src/fast_select/__init__.py
@@ -0,0 +1,6 @@
+from .ReliefF import ReliefF
+from .SURF import SURF
+from .MultiSURF import MultiSURF
+from .chi2 import chi2
+
+__all__ = ['ReliefF', 'SURF', 'MultiSURF', 'chi2']
