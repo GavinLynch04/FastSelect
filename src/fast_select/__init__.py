e65c25f9dfc78828f4bc560eca2c4382e4d84965
Gavin Lynch <34323734+GavinLynch04@users.noreply.github.com>
2025-07-21 14:20:36 -0400

    Update __init__.py

--- src/fast_select/__init__.py
+++ src/fast_select/__init__.py
@@ -1,6 +1,5 @@
 from .ReliefF import ReliefF
 from .SURF import SURF
 from .MultiSURF import MultiSURF
-from .chi2 import chi2
 
-__all__ = ['ReliefF', 'SURF', 'MultiSURF', 'chi2']
+__all__ = ['ReliefF', 'SURF', 'MultiSURF']
