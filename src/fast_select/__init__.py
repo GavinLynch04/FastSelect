aaf03bd4679b91c6e73016be8d06d84868006c1b
Gavin Lynch <gavinlynch04@gmail.com>
2025-08-01 11:35:55 -0700

    wrapping up changes for v0.2.0, updated changelog/readme/init

--- src/fast_select/__init__.py
+++ src/fast_select/__init__.py
@@ -4,5 +4,7 @@ from .MultiSURF import MultiSURF
 from .TuRF import TuRF
 from .mRMR import mRMR
 from .Chi2 import chi2
+from .MDR import MDR
+from .CFS import CFS
 
-__all__ = ['ReliefF', 'SURF', 'MultiSURF', 'TuRF', 'mRMR', 'chi2']
+__all__ = ['ReliefF', 'SURF', 'MultiSURF', 'TuRF', 'mRMR', 'chi2', 'MDR', 'CFS']
